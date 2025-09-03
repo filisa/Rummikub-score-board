f2cbeccadd36b2f61489375b1e1f55d9a6ef6662
Margarita Nikulina <ritan@fr24.com>
2025-09-03 22:21:14 +0200

    edit header
    added a header and an option to edit the name of the board

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 //1 changing the name of the board should be in LocalStorage
 //add 3 participants 
 //store scores and names in Local Storage
+const boardName = document.getElementById("leaderboard_title")
+const changeName = document.getElementById("change_board_name")
+const okChangeBtn = document.getElementById("ok_change_btn")
+const inputChangeName = document.getElementById("leaderboard_title_change")
+
+boardName.addEventListener('click', function(){
+    boardName.style.display = 'none'
+    changeName.style.display = 'inline'
+    inputChangeName.focus()
+})
+
+okChangeBtn.addEventListener('click', function(){
+    boardName.textContent = inputChangeName.value
+    changeName.style.display = 'none'
+    boardName.style.display = 'inline'
+    localStorage.setItem("name", inputChangeName.value) 
+})
+
+//local storage stil doesn't work yet! fix
